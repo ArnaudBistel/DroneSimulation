d882bca5184797ece372a3baf3fd6873a4d707be
Hydriss <hydros.kirichi@gmail.com>
2021-11-21 14:14:30 +0100

    add basis of SimulatedAnnealing solver

--- src/main/sarl/solutionSolver/simulatedAnnealing/SimulatedAnnealing.java
+++ src/main/sarl/solutionSolver/simulatedAnnealing/SimulatedAnnealing.java
@@ -18,20 +18,38 @@ public class SimulatedAnnealing extends SolutionSolver {
 	public List<List<MapPoint>> Solve() {
 		//Fonction solve avec des paramètres par défauts
 		
-		return Solve(5.0, 0.5);
+		return Solve(5.0, 0.5, 0.9, 5);
 	}
 
-	public List<List<MapPoint>> Solve(double t, double treshold) {
+	public List<List<MapPoint>> Solve(double tempInit, double treshold, double alpha, int nbItPalier) {
 		//résolution par recuit simulé
 		
+		//initialisation
+		double t = tempInit;
 		List<List<MapPoint>> s = getInitialSolution(); //s initialiser a la solution initial
 		
-		return null;
+		//boucle sur la temperature
+		while(t > treshold) {
+			//boucle sur n iteration par palier de temperature
+			for(int i = 0; i < nbItPalier; i++) {
+				//selection d'un voisin de s
+				List<List<MapPoint>> neighbor = getRandomNeighbor(s);
+				
+				//test si le voisin est garder
+				
+				
+			}
+			
+			//reduction de la temperature
+			t = t * alpha;
+		}
+		
+		return s;
 	}
 	
 	private List<List<MapPoint>> getInitialSolution() {
 		//Pour chaque point de livraisons, on créer une liste contenant :
-		// le depot le plus proche -> le point -> le depot le plus proche
+		//le depot le plus proche -> le point -> le depot le plus proche
 		
 		List<List<MapPoint>> res = new ArrayList<List<MapPoint>>();
 		for(MapPoint p : map.getClients()) {
@@ -43,6 +61,10 @@ public class SimulatedAnnealing extends SolutionSolver {
 			res.add(list);
 		}
 		return res;
+	}
+	
+	private List<List<MapPoint>> getRandomNeighbor(List<List<MapPoint>> s) {
+		return null;
 	}
 	
 	static private MapPoint getClosestWharehouse(MapPoint p) {
