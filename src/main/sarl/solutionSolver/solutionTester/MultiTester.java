3812a47128c02dcb09ecdd6d8f48f1d24690dc9f
Hydriss <hydros.kirichi@gmail.com>
2021-12-23 23:05:57 +0100

    add Multi test simulated annealing

--- src/main/sarl/solutionSolver/solutionTester/MultiTester.java
+++ src/main/sarl/solutionSolver/solutionTester/MultiTester.java
@@ -1,7 +1,9 @@
 package solutionSolver.solutionTester;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import map.MapPoint;
 import map.SimulationMap;
 
 public abstract class MultiTester {
@@ -9,22 +11,31 @@ public abstract class MultiTester {
 	protected SimulationMap map;
 	protected int nbStep;
 	protected int nbTestByStep;
+	protected int nbDrones;
 	
 	protected List<Double> energyCostResults;
 	protected List<Double> timeCostResults;
+	protected List< List<List<MapPoint>> > solutionsHistory; 
 	
-	protected int i = 0; //valeur variant entre 0 et nbStep
-	protected int j = 0; //valeur variant entre 0 et nbTestByStep
+	protected double i = 0; //valeur variant entre 0 et nbStep
+	protected double j = 0; //valeur variant entre 0 et nbTestByStep
 	
-	public MultiTester(SimulationMap map, int nbStep) {
+	public MultiTester(SimulationMap map, int nbStep, int nbTestByStep, int nbDrones) {
 		this.map = map;
-	}
+		this.nbDrones = nbDrones;
+		this.nbStep = nbStep;
+		this.nbTestByStep = nbTestByStep;
+		this.energyCostResults = new ArrayList<>();
+		this.timeCostResults = new ArrayList<>();
+		this.solutionsHistory = new ArrayList<>();
+	} 
 	
 	public void runMultiTest() {
 		while(!runMultiTestStep());
 	}
 	
-	public abstract boolean runMultiTestStep();
+	public abstract boolean runMultiTestStep(); 
+	//renvoie true si le multiTest est terminer
 
 	public int getNbStep() {
 		return nbStep;
@@ -36,6 +47,36 @@ public abstract class MultiTester {
 
 	public List<Double> getTimeCostResults() {
 		return timeCostResults;
+	}
+	
+	public List< List<List<MapPoint>> > getSolutionsHistory() {
+		return solutionsHistory;
+	}
+	
+	public double getAvancement() {
+		return ((double)i * (double)nbTestByStep + (double)j) / ((double)nbStep * (double)nbTestByStep);
+	}
+	
+	protected double meanOfList(List<Double> list) {
+		//fait la moyenne des doubles d'une liste
+		double v = 0;
+		for (Double e : list) {
+			v += e;
+		}
+		return v / list.size();
+	}
+	
+	protected int bestSolution(List<Double> energyCost){
+		//récupère l'id de la mailleur solution d'une liste
+		int id = 0;
+		double minCost = energyCost.get(0);
+		for(int i = 1; i < energyCost.size(); i++) {
+			if(energyCost.get(i) < minCost) {
+				id = i;
+				minCost = energyCost.get(i);
+			}
+		}
+		return id;
 	}
 	
 	
--- src/main/sarl/solutionSolver/solutionTester/MultiTesterSimulatedAnnealing.java
+++ src/main/sarl/solutionSolver/solutionTester/MultiTesterSimulatedAnnealing.java
@@ -4,6 +4,8 @@ import map.MapPoint;
 import map.SimulationMap;
 import solutionSolver.SolutionSolver;
 import solutionSolver.simulatedAnnealing.SimulatedAnnealing;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MultiTesterSimulatedAnnealing extends MultiTester {
@@ -12,30 +14,77 @@ public class MultiTesterSimulatedAnnealing extends MultiTester {
 	double minTempInit, maxTempInit;
 	double minTreshold, maxTreshold;
 	double minAlpha, maxAlpha;
-	int minNbItPalier, maxNbItPalier;
+	double minNbItPalier, maxNbItPalier;
 
-	public MultiTesterSimulatedAnnealing(SimulationMap map, int nbStep,
+	List<Double> currentEnergyCosts;
+	List<Double> currentTimeCosts;
+	List<List<List<MapPoint>>> currentSolutions;
+
+	public MultiTesterSimulatedAnnealing(SimulationMap map, int nbStep, int nbTestByStep, int nbDrones,
 			double minTempInit, double maxTempInit,
 			double minTreshold, double maxTreshold,
 			double minAlpha, double maxAlpha,
 			int minNbItPalier, int maxNbItPalier){
-		super(map, nbStep);
+		
+		super(map, nbStep, nbTestByStep, nbDrones);
 		sim = new SimulatedAnnealing(map);
+		this.minTempInit = minTempInit;
+		this.maxTempInit = maxTempInit;
+		this.minTreshold = minTreshold;
+		this.maxTreshold = maxTreshold;
+		this.minAlpha = minAlpha;
+		this.maxAlpha = maxAlpha;
+		this.minNbItPalier = minNbItPalier;
+		this.maxNbItPalier = maxNbItPalier;
+
+		this.currentEnergyCosts = new ArrayList<>();
+		this.currentTimeCosts = new ArrayList<>();
+		this.currentSolutions = new ArrayList<>();
+		
 	}
 	
 	@Override
 	public boolean runMultiTestStep() {
 
-		/*double currentTempInit = minTempInit + (  i * (maxTempInit - minTempInit) / this.nbStep);
+		//calcul des paramètres pour l'étape actuelle
+		double currentTempInit = minTempInit + (  i * (maxTempInit - minTempInit) / this.nbStep);
 		double currentTreshold = minTreshold + (  i * (maxTreshold - minTreshold) / this.nbStep);
 		double currentAlpha = minAlpha + (  i * (maxAlpha - minAlpha) / this.nbStep);
-		int currentNbItPalier = minNbItPalier + (  i * (maxNbItPalier - minNbItPalier) / this.nbStep);
-		List<List<MapPoint>> currentSolution =
-				sim.Solve(currentTempInit, currentTreshold, currentAlpha, currentNbItPalier);
-		double Energycost = SolutionSolver.solutionCost(currentSolution);
-		double timeCost = SolutionSolver.solutionTimeCost(solution, nDrone)
-		return i * nbTestByStep + j >= nbStep * nbTestByStep;*/
-		return true;
+		double currentNbItPalier = minNbItPalier + (  i * (maxNbItPalier - minNbItPalier) / this.nbStep);
+		
+		//calcul de la solution
+		List<List<MapPoint>> sol =
+				sim.Solve(currentTempInit, currentTreshold, currentAlpha, (int)currentNbItPalier);
+		
+		//récupération des résulats
+		double energycost = SolutionSolver.solutionCost(sol);
+		double timeCost = SolutionSolver.solutionTimeCost(sol, nbDrones);
+		this.currentEnergyCosts.add(energycost);
+		this.currentTimeCosts.add(timeCost);
+		this.currentSolutions.add(sol);
+		
+		j++;
+		if(j >= nbTestByStep) { //si tout les tests d'une étapes ont été fait
+			
+			//calcul des moyennes
+			this.energyCostResults.add(meanOfList(currentEnergyCosts));
+			this.timeCostResults.add(meanOfList(currentTimeCosts));
+			
+			//récupération de la meilleur solution de l'étape
+			int idxBestSolution = bestSolution(currentEnergyCosts);
+			this.solutionsHistory.add(currentSolutions.get(idxBestSolution));
+			
+			//nettoyage des listes
+			this.currentEnergyCosts.clear();
+			this.currentTimeCosts.clear();
+			this.currentSolutions.clear();
+			j = 0;
+			i++;
+		}
+		
+		return getAvancement() >= 1;
 	}
+	
+	
 
 }
